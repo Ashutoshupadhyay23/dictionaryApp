4cd1564d151546a09eb2ef973d11f9922a23de18
Ashutosh Upadhyay <upadhyayashutosh423@gmail.com>
2024-03-18 09:58:27 +0530

    worked on definition page

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,31 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import Header from './component/header/Header'
 
-import { Container } from '@mui/material'
+import { Container, Switch } from '@mui/material'
+import {withStyles} from '@mui/material/styles'
 import axios from 'axios'
+import Definition from './component/definitions/Definition'
+import { grey } from '@mui/material/colors'
 
 function App() {
 
   const [meanings, setMeanings] = useState([])
   const [word, setWord] = useState("")
   const [category, setCategory] = useState("en")
+
+  const purpleSwitch = withStyles({
+    swichBase: {
+      color: grey[300],
+      "&$checked" : {
+        color: grey[500],
+      },
+      "&$checked + $track" : {
+        backgroundColor: grey[500],
+      },
+    },
+    checked: {},
+    track: {},
+  })(Switch);
 
   const dictionaryApi = async () => {
     try {
@@ -35,6 +52,12 @@ function App() {
         maxWidth='md'
         style={{display: 'flex', flexDirection: 'column', height: '100vh'}}
       >
+
+        <div 
+          style={{position: 'absolute', top: 0, right: 15, paddingTop: 10}}
+        >
+          theme
+        </div>
         
         <Header 
           category={category} 
@@ -42,6 +65,13 @@ function App() {
           word={word} 
           setWord={setWord} 
         />
+
+       {meanings && (
+        <Definition 
+          word={word} 
+          meanings={meanings} 
+          category={category} 
+        />)}
 
       </Container>
     </div>
---
+++ src/component/definitions/Definition.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import './Definition.css'
+
+const Definition = ({ word, category, meanings }) => {
+  return (
+    <div className='meanings'>
+
+        {meanings[0] && word && category === 'en' && (
+            <audio
+                src={meanings[0].phonetics[0] && meanings[0].phonetics[0].audio}
+                controls
+                style={{backgroundColor: '#fff', borderRadius: 10}}
+            >
+                Your browser does'nt support audio element.
+            </audio>
+        )}
+        
+        {word === "" ? <span className='subTitle'>
+            Start by typing a word in searchbox.
+        </span> : (
+            meanings.map((mean) => {
+                mean.meanings.map((item) => {
+                    item.definitions.map((def) => {
+                        <div 
+                            className='singleMean' 
+                            style={{ backgroundColor: 'white', color: 'black'}}
+                        >
+                            <b> {def.definition} </b>
+                            <hr style={{ backgroundColor: 'black', width: '100%'}} />
+
+                            
+                            {def.example && (
+                                <span>
+                                    <b>Example:</b>
+                                    {def.example}
+                                </span>
+                            )}
+
+                            {def.synonyms && (
+                                <span>
+                                    <b>Synonym:</b>
+                                    {def.synonyms.map((s) => {
+                                        `${s}, `
+                                    })}
+                            </span>
+                            )}
+                            
+                        </div>
+                    })
+                })
+            })
+        )}
+        
+    </div>
+  )
+}
+
+export default Definition
